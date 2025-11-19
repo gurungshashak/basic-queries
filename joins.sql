526ba55533aaeda60db66ad6443689feefbfb023
gurungshashak <gurungshashak@gmail.com>
2025-11-19 14:47:17 -0500

    Add files via upload

---
+++ joins.sql
@@ -0,0 +1,66 @@
+USE northwind;
+
+-- 1. List the product id, product name, unit price and category name of all
+-- products. Order by category name and within that, by product name.
+
+SELECT ProductID, ProductName, UnitPrice, CategoryName
+FROM products  AS p
+JOIN categories AS c
+  ON p.CategoryID = c.CategoryID
+ORDER BY c.CategoryName, p.ProductName;  
+
+
+-- 2. List the product id, product name, unit price and supplier name of all
+-- products that cost more than $75. Order by product name.
+
+SELECT ProductID, ProductName, UnitPrice, ContactName
+FROM products AS p
+JOIN suppliers AS s
+  ON p.SupplierID = s.SupplierID
+Where p.UnitPrice > 75
+ORDER BY p.ProductName;  
+
+
+
+-- 3. List the product id, product name, unit price, category name, and supplier
+-- name of every product. Order by product name.
+
+SELECT ProductID, ProductName, UnitPrice, CategoryName, ContactName
+FROM products AS p
+JOIN categories As c
+ ON p.ProductID = c.CategoryID
+JOIN suppliers AS s
+  ON p.SupplierID = s.SupplierID
+ORDER BY p.ProductName;  
+
+-- 4. What is the product name(s) and categories of the most expensive
+-- products? HINT: Find the max price in a subquery and then use that in
+-- your more complex query that joins products with categories.
+
+SELECT max(UnitPrice) From products;
+
+SELECT ProductName, CategoryName
+FROM products AS p
+JOIN categories AS c
+ ON p.ProductID = c.CategoryID
+WHERE UnitPrice = (SELECT max(UnitPrice) From products); 
+
+
+
+-- 5. List the order id, ship name, ship address, and shipping company name of
+-- every order that shipped to Germany.
+
+SELECT OrderID, ShipName, ShipAddress, CompanyName 
+FROM orders AS o
+JOIN shippers AS s
+ on o.ShipVia = s.ShipperID
+Where o.ShipCountry = "Germany"; 
+
+
+-- 6. List the order id, order date, ship name, ship address of all orders that
+-- ordered "Sasquatch Ale"?  
+SELECT OrderID, OrderDate, ShipName, ShipAddress
+FROM ordersproducts
+
+   
+
